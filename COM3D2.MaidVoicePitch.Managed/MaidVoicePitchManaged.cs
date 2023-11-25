a65da86c3a13aa32c23c2bcaf392a6adb6cd0b1a
Perdition <87424475+Perdition-117@users.noreply.github.com>
2023-11-25 12:30:32 +0100

    Initial commit

---
+++ COM3D2.MaidVoicePitch.Managed/MaidVoicePitchManaged.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace CM3D2.MaidVoicePitch.Managed
+{
+	namespace Callbacks
+	{
+		namespace TBody
+		{
+
+			public static class LateUpdate
+			{
+				public delegate void Callback(global::TBody that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::TBody that)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+
+			public static class MoveHeadAndEye
+			{
+				public delegate void Callback(global::TBody that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::TBody that)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+		}
+
+		namespace BoneMorph_
+		{
+			public static class Blend
+			{
+				public delegate void Callback(global::BoneMorph_ that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::BoneMorph_ that)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+		}
+
+		namespace AudioSourceMgr
+		{
+			public static class Play
+			{
+				public delegate void Callback(global::AudioSourceMgr that, float f_fFadeTime, bool loop);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::AudioSourceMgr that, float f_fFadeTime, bool loop)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that, f_fFadeTime, loop);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+
+			public static class PlayOneShot
+			{
+				public delegate void Callback(global::AudioSourceMgr that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::AudioSourceMgr that)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+		}
+
+		namespace CharacterMgr
+		{
+			public static class PresetSet
+			{
+				public delegate void Callback(global::CharacterMgr that, Maid f_maid, global::CharacterMgr.Preset f_prest);
+
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::CharacterMgr that, Maid f_maid, global::CharacterMgr.Preset f_prest, bool f_man)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that, f_maid, f_prest);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+		}
+
+		namespace DynamicSkirtBone
+		{
+			public static class PreUpdateSelf
+			{
+				public delegate void Callback(global::DynamicSkirtBone that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::DynamicSkirtBone that)
+				{
+
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+
+			public static class PostUpdateSelf
+			{
+				public delegate void Callback(global::DynamicSkirtBone that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::DynamicSkirtBone that)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+		}
+
+		namespace jiggleBone
+		{
+			public static class PreLateUpdateSelf
+			{
+				public delegate void Callback(global::jiggleBone that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::jiggleBone that)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+		}
+
+		namespace jiggleBone
+		{
+			public static class PostLateUpdateSelf
+			{
+				public delegate void Callback(global::jiggleBone that);
+				public static Callbacks<Callback> Callbacks = new Callbacks<Callback>();
+
+				public static void Invoke(global::jiggleBone that)
+				{
+					try
+					{
+						for (int i = 0; i < Callbacks.ArrayOfVals.Length; i++)
+						{
+							Callbacks.ArrayOfVals[i](that);
+						}
+					}
+					catch (Exception e)
+					{
+						Helper.ShowException(e);
+					}
+				}
+			}
+		}
+		//Calling values in a normal dictionary returns an enumerable which has high GC allocs. Given the amount of calls for that property, we made a cached dictionary that does basically the same thing but way faster without any GC allocs.
+		public class Callbacks<TValue> : SortedDictionary<string, TValue>
+		{
+			private TValue[] values = new TValue[0];
+
+			public TValue[] ArrayOfVals
+			{
+				get
+				{
+					if (base.Count != values.Length)
+					{
+						//Console.WriteLine("Updated ArrayOfVals");
+
+						Array.Resize(ref values, Count);
+						base.Values.CopyTo(values, 0);
+					}
+
+					return values;
+				}
+			}
+		}
+	}
+}
---
+++ COM3D2.MaidVoicePitch.Plugin/ForeArmFix.cs
@@ -0,0 +1,96 @@
+﻿using CM3D2.ExternalSaveData.Managed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityInjector;
+
+namespace CM3D2.MaidVoicePitch.Plugin
+{
+	class MaidForearms
+	{
+		public Transform LeftForeArm;
+		public Transform RightForeArm;
+		public Transform UpperArm;
+	}
+	//The original Farmfix gave dry turds a run for their money with some of the largest GC Allocs in meido causing massive stutter. This resolves the situation completely, making FarmFix 0 alloc.
+	internal static class ForeArmFixOptimized
+	{
+		static Dictionary<string, MaidForearms> maidBones = new Dictionary<string, MaidForearms>();
+
+		// ForeArmFix : 前腕の歪みを修正する CM3D.MaidVoicePitch.Plugin.cs の追加メソッド
+		// CM3D.MaidVoicePitch.Plugin を適用しメイドのフリーコメント欄に #FARMFIX# の記述で前腕の歪みを修正する。
+		// 前腕歪みバグを修正
+		internal static void ForeArmFix(Maid maid)
+		{
+			if (!ExSaveData.GetBool(maid, MaidVoicePitch.PluginName, "FARMFIX", false))
+			{
+				return;
+			}
+
+			BoneMorph_ bm_ = maid.body0.bonemorph;
+
+			if (!maidBones.ContainsKey(maid.status.guid) || maidBones[maid.status.guid].LeftForeArm == null || maidBones[maid.status.guid].RightForeArm == null || maidBones[maid.status.guid].UpperArm == null) 
+			{
+				int i = 0;
+
+				Transform LFore = null;
+				Transform RFore = null;
+				Transform UppArm = null;
+
+				for (i = 0; i < bm_.bones.Count; i++)
+				{
+					if (bm_.bones[i].linkT == null) continue;
+					if (bm_.bones[i].linkT.name == "Bip01 L Forearm") LFore = bm_.bones[i].linkT;
+					if (bm_.bones[i].linkT.name == "Bip01 R Forearm") RFore = bm_.bones[i].linkT;
+					if (bm_.bones[i].linkT.name == "Bip01 L UpperArm") UppArm = bm_.bones[i].linkT;
+
+					if (LFore != null && RFore != null && UppArm != null) 
+					{
+						break;
+					}
+				}
+
+				if (LFore != null && RFore != null && UppArm != null)
+				{
+					maidBones[maid.status.guid] = new MaidForearms
+					{
+						LeftForeArm = LFore,
+						RightForeArm = RFore,
+						UpperArm = UppArm
+					};
+				}
+				else 
+				{
+					return;
+				}
+			}
+
+			var Package = maidBones[maid.status.guid];
+			var sclUAx = Package.UpperArm.localScale.x;
+
+			Vector3 sclUA = new Vector3(sclUAx, 1f, 1f);
+
+			Vector3 antisclUA_d = new Vector3(1f / sclUAx - 1f, 0f, 0f);
+
+			Vector3 eaFAL = Package.LeftForeArm.localRotation.eulerAngles;
+			Vector3 eaFAR = Package.RightForeArm.localRotation.eulerAngles;
+
+			Quaternion antirotFAL = Quaternion.Euler(eaFAL - new Vector3(180f, 180f, 180f));
+			Quaternion antirotFAR = Quaternion.Euler(eaFAR - new Vector3(180f, 180f, 180f));
+			Vector3 sclFAL_d = antirotFAL * antisclUA_d;
+			Vector3 sclFAR_d = antirotFAR * antisclUA_d;
+
+			Vector3 antisclFAL = new Vector3(1f, 1f, 1f) + new Vector3(Mathf.Abs(sclFAL_d.x), Mathf.Abs(sclFAL_d.y), Mathf.Abs(sclFAL_d.z));
+			Vector3 antisclFAR = new Vector3(1f, 1f, 1f) + new Vector3(Mathf.Abs(sclFAR_d.x), Mathf.Abs(sclFAR_d.y), Mathf.Abs(sclFAR_d.z));
+
+			//foreach (Transform t in tListFAL) t.localScale = Vector3.Scale(antisclFAL, sclUA);
+
+			Package.LeftForeArm.localScale = Vector3.Scale(antisclFAL, sclUA);
+
+			//foreach (Transform t in tListFAR) t.localScale = Vector3.Scale(antisclFAR, sclUA);
+			Package.RightForeArm.localScale = Vector3.Scale(antisclFAR, sclUA);
+		}
+	}
+}
